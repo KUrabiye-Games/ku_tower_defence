e971e98779f00d487ca0c91a711ac0aa8220ceb0
atlaspolat <apolat21@ku.edu.tr>
2025-05-09 18:45:52 +0300

    ICollectable

---
+++ model/src/main/java/com/kurabiye/kutd/model/Collectable/ICollectable.java
@@ -0,0 +1,18 @@
+package com.kurabiye.kutd.model.Collectable;
+
+/*  ICollectable.java
+ *  This interface is used to define a collectable item.
+ *  It is a generic interface that can be used with any type of item.
+ *  
+ *  @author: Atlas Berk Polat
+ *  @version: 2.0
+ *  @since: 2025-09-25
+ */
+
+public interface ICollectable<T> {
+
+
+    T getItem();
+
+
+}
