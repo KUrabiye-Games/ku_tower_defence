bb7748170afe409adbed7de4e679629c2d09bd99
jsho <jsho@itu.dk>
2025-06-16 23:54:18 +0300

    feat: set logo for title/window bar

--- view/src/main/java/com/kurabiye/kutd/view/View.java
+++ view/src/main/java/com/kurabiye/kutd/view/View.java
@@ -1,17 +1,17 @@
 package com.kurabiye.kutd.view;
 
 import javafx.application.Application;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class View extends Application {
 
     @Override
     public void start(Stage stage) {
+        Image image = new Image(getClass().getResourceAsStream("/assets/cookie_bitten1.png"));
+        stage.getIcons().add(image);
+
         // Start the Launch Screen
-        LaunchScreenView launchScreen = new LaunchScreenView();
         launchScreen.start(stage);
-        
     }
 
     public static void main(String[] args) {
